420956b88ddcade1e4668ad9a1cd2554055aeeb8
Rifqialba <rifqialba12@gmail.com>
2026-05-24 15:37:16 +0700

    feat: add database migration system and users table

--- apps/server/database/migrations/20260524081225_create_users_table.up.sql
+++ apps/server/database/migrations/20260524081225_create_users_table.up.sql
@@ -17,9 +17,3 @@ CREATE TABLE users (
 
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
-
-CREATE INDEX idx_users_email
-ON users(email);
-
-CREATE INDEX idx_users_username
-ON users(username);
